f7d40db9cc44f8f617e30d9b6a98525bdf9a307e
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-16 15:30:01 +0100

    Init the PW

---
+++ java/travauxPratiques/Cercle.java
@@ -0,0 +1,19 @@
+class Cercle extends Forme {
+  private double rayon;
+  private static final double PI = 3.14159;
+
+  Cercle(double rayon, String couleur, double x, double y) {
+    super(couleur, x, y);
+    this.rayon = rayon;
+  }
+
+  public double calculerAire() {
+    return PI * (rayon * rayon);
+  }
+
+  public double calculerPerimetre() {
+    return 2 * PI * rayon;
+  }
+
+  public double getDiametre() { return 2 * rayon; }
+}
